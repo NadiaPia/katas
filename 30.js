caff8d7a9597ac5fc8a371b9fc755ceb8b8c0c36
NadiaPia <206i417@gmail.com>
2023-09-28 00:05:25 -0700

    Add another way to HumanReadible

--- 30.js
+++ 30.js
@@ -1,48 +1,38 @@
-function humanReadable (seconds) {
+// function humanReadable (seconds) {
 
-    function transformation (arg) {
-        let argStr = arg.toString();    
-        argStr = argStr.length < 2 ? "0" + argStr : argStr;
-        return argStr;
-    };
+//     function transformation (arg) {
+//         let argStr = arg.toString();
+//         argStr = argStr.length < 2 ? "0" + argStr : argStr;
+//         return argStr;
+//     };
     
-    let HH = Math.trunc(seconds / 3600);
-    let reminder =  seconds - (3600 * HH);
-    let MM = Math.trunc(reminder / 60);
-    let SS = reminder - (MM * 60);
+//     let HH = Math.trunc(seconds / 3600);
+//     let reminder =  seconds - (3600 * HH);
+//     let MM = Math.trunc(reminder / 60);
+//     let SS = reminder - (MM * 60);
 
-    const result = `${transformation(HH)}:${transformation(MM)}:${transformation(SS)}`
-    console.log(typeof(result))
+//     const result = `${transformation(HH)}:${transformation(MM)}:${transformation(SS)}`
+//     //console.log(typeof(result))
  
-    return (result);    
-}
-
-//console.log(humanReadable(45296));
-//console.log(humanReadable(90));
-//console.log(humanReadable(0));
-console.log(humanReadable(120321));
-
-
-
-
-
-// function transformation (arg) {
-//     let argStr = arg.toString();    
-//     argStr = argStr.length < 2 ? "0" + argStr : argStr;
-//     return argStr;
+//     return (result);    
 // }
 
-// console.log(transformation(9))
-
-// const MM = 3;
-// let MMstr = MM.toString()
-
-// MMstr = MMstr.length < 2 ? "0" + MMstr : MMstr
-
-// console.log(MMstr)
+// console.log(humanReadable(45296));
+// console.log(humanReadable(90));
+// console.log(humanReadable(0));
+// console.log(humanReadable(120321));  
 
 
+function humanReadable (seconds) {
+    return [seconds / 3600, seconds % 3600 / 60, seconds % 60].map((v) => {
+        v = Math.trunc(v).toString();
+        return v.length < 2 ? "0" + v : v
+    }).join(":")
+
+    
+}
+console.log(humanReadable(45296));
+console.log(humanReadable(90));
 
 
 
-  
